a7b89957740ae387fe35d3d14fa4ec89b36ba3b6
ilovapples <89090508+ilovapples@users.noreply.github.com>
2022-11-27 09:03:04 -0600

    Updated Tic Tac Toe file.

--- ticTacToe-1.2.py
+++ ticTacToe-1.2.py
@@ -1,18 +1,34 @@
 import os
+import platform
+clearValue = ""
+if platform.system == "Windows":
+    clearValue = 'cls'
+elif platform.system == "Darwin":
+    clearValue = 'clear'
+
+
 def clear():
-    os.system('cls')
+    os.system(clearValue)
+
+
 board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+
+
 def printBoard():
     clear()
     print("_____________")
     print("|_%s_|_%s_|_%s_|" % (board[0], board[1], board[2]))
     print("|_%s_|_%s_|_%s_|" % (board[3], board[4], board[5]))
     print("|_%s_|_%s_|_%s_|" % (board[6], board[7], board[8]))
+
+
 printBoard()
 stillPlaying = "y"
 turn = 1
 xoroTurn = "X"
 wasxoroTurn = ""
+
+
 def gameEnd():
     stillPlaying = input("Play Again? (y/n): ")
     if stillPlaying == "y":
@@ -28,30 +44,35 @@ def gameEnd():
         printBoard()
     elif stillPlaying == "n":
         quit()
+
+
 while stillPlaying == "y":
     move = input("Move: ")
-    if move == "r":
-        board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
-        printBoard()
-    elif move == "":
-        print("Invalid Move.")
-    elif move == "help":
-        print("Commands are:\n\n\"r\" to reset board\nAny number 1 to 9 to choose where to put your piece.\n\"?\" or \"help\" to bring up the help menu.")
-    elif move == "?":
-        print("Commands are:\n\n\"r\" to reset board\nAny number 1 to 9 to choose where to put you piece.\n\"?\" or \"help\" to bring up the help menu.")
-    elif int(move) > 9:
-        print("Invalid Move.")
-    elif int(move) < 1:
-        print("Invalid Move.")
-    elif board[int(move)-1] != "_":
-        print("Invalid Move.")
-    else:
+    try:
+        if int(move) > 9:
+            print("Invalid Move.")
+        elif int(move) < 1:
+            print("Invalid Move.")
+        if board[int(move)-1] != "_":
+            print("Invalid Move.")
+    except ValueError:
+        if move == "r":
+            board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+            printBoard()
+        elif move == "":
+            print("Invalid Move.")
+        elif move == "help":
+            print("Commands are:\n\n\"r\" to reset board\nAny number 1 to 9 to choose where to put your piece.\n\"?\" or \"help\" to bring up the help menu.")
+        elif move == "?":
+            print("Commands are:\n\n\"r\" to reset board\nAny number 1 to 9 to choose where to put you piece.\n\"?\" or \"help\" to bring up the help menu.")
+
+    try:
         board[int(move)-1] = xoroTurn
         wasxoroTurn = xoroTurn
         turn += 1
-        if (turn%2) == 0:
+        if (turn % 2) == 0:
             xoroTurn = "O"
-        elif (turn%2) != 0:
+        elif (turn % 2) != 0:
             xoroTurn = "X"
         printBoard()
         if board[int(move)-1] is board[0]:
@@ -66,7 +87,7 @@ while stillPlaying == "y":
                     print("%s got 3 in a row!" % wasxoroTurn)
                     gameEnd()
         if board[int(move)-1] is board[1]:
-            if board[1] == wasxoroTurn:    
+            if board[1] == wasxoroTurn:
                 if board[1] == board[4] == board[7]:
                     print("%s got 3 in a row!" % wasxoroTurn)
                     gameEnd()
@@ -81,3 +102,13 @@ while stillPlaying == "y":
                 if board[0] == board[1] == board[2]:
                     print("%s got 3 in a row!" % wasxoroTurn)
                     gameEnd()
+    except ValueError:
+        if move == "r":
+            board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+            printBoard()
+        elif move == "":
+            print("Invalid Move.")
+        elif move == "help":
+            print("Commands are:\n\n\"r\" to reset board\nAny number 1 to 9 to choose where to put your piece.\n\"?\" or \"help\" to bring up the help menu.")
+        elif move == "?":
+            print("Commands are:\n\n\"r\" to reset board\nAny number 1 to 9 to choose where to put you piece.\n\"?\" or \"help\" to bring up the help menu.")
