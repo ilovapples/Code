5e813f21f254015081fd2747b874cbb0056d9456
ilovapples <89090508+ilovapples@users.noreply.github.com>
2022-12-02 19:27:45 -0600

    Added fileAccess.py
    Added fileAccess.py to read, write or append text to files.

---
+++ fileAccess.py
@@ -0,0 +1,19 @@
+def accessFile(filename, text, readWriteAppend):
+    if readWriteAppend.lower() == "write":
+        write(filename, text)
+    elif readWriteAppend.lower() == "read":
+        print(read(filename))
+    elif readWriteAppend.lower() == "append":
+        append(filename, text)
+def write(filename, text):
+    with open(filename, "w") as fileObj:
+        fileObj.write(text)
+        
+def read(filename):
+    with open(filename, "r") as fileObj:
+        return fileObj.read()
+    
+def append(filename, text):
+    with open(filename, "a") as fileObj:
+        fileObj.write(text)
+accessFile(input("Filename: "), input("Text input for Write or Append (input doesn't matter if reading): "), input("Read, Write, or Append: "))
