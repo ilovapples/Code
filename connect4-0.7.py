fad53431f8c7cfec62893fb0ba42ecc15de329a6
ilovapples <89090508+ilovapples@users.noreply.github.com>
2022-12-21 12:00:52 -0600

    Update connect4-0.7.py

--- connect4-0.7.py
+++ connect4-0.7.py
@@ -1,62 +1,39 @@
 import simple_colors
-slot0 = ["__", "__", "__", "__", "__", "__"]
-slot1 = ["__", "__", "__", "__", "__", "__"]
-slot2 = ["__", "__", "__", "__", "__", "__"]
-slot3 = ["__", "__", "__", "__", "__", "__"]
-slot4 = ["__", "__", "__", "__", "__", "__"]
-slot5 = ["__", "__", "__", "__", "__", "__"]
-slot6 = ["__", "__", "__", "__", "__", "__"]
-slot0Indexing = 0
-slot1Indexing = 0
-slot2Indexing = 0
-slot3Indexing = 0
-slot4Indexing = 0
-slot5Indexing = 0
-slot6Indexing = 0
+board = [
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+]
+col1Indx = 5
+col2Indx = 5
+col3Indx = 5
+col4Indx = 5
+col5Indx = 5
+col6Indx = 5
+col7Indx = 5
 moveIsInt = 1
-moveIs = "🔵"
+moveIs = "X"
 isFull = [" ", " ", " ", " ", " ", " ", " "]
 def printBoard():
-    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (slot0[5], slot1[5], slot2[5], slot3[5], slot4[5], slot5[5], slot6[5]))
-    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (slot0[4], slot1[4], slot2[4], slot3[4], slot4[4], slot5[4], slot6[4]))
-    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (slot0[3], slot1[3], slot2[3], slot3[3], slot4[3], slot5[3], slot6[3]))
-    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (slot0[2], slot1[2], slot2[2], slot3[2], slot4[2], slot5[2], slot6[2]))
-    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (slot0[1], slot1[1], slot2[1], slot3[1], slot4[1], slot5[1], slot6[1]))
-    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (slot0[0], slot1[0], slot2[0], slot3[0], slot4[0], slot5[0], slot6[0]))
-    print("  %s   %s   %s   %s   %s   %s   %s" % (isFull[0], isFull[1], isFull[2], isFull[3], isFull[4], isFull[5], isFull[6]))
+    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
+    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
+    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
+    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
+    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
+    print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
+    print("  %s  %s  %s  %s  %s  %s  %s" % (isFull[0], isFull[1], isFull[2], isFull[3], isFull[4], isFull[5], isFull[6]))
 printBoard()
 
 
 while True:
     if (moveIsInt%2) == 0:
-        moveIs = "🔴"
+        moveIs = "O"
     elif (moveIsInt%2) != 0:
-        moveIs = "🔵"
-    if slot0Indexing > 5:
-        isFull[0] = "FL"
-        printBoard()
-    if slot1Indexing > 5:
-        isFull[1] = "FL"
-        printBoard()
-    if slot2Indexing > 5:
-        isFull[2] = "FL"
-        printBoard()
-    if slot3Indexing > 5:
-        isFull[3] = "FL"
-        printBoard()
-    if slot4Indexing > 5:
-        isFull[4] = "FL"
-        printBoard()
-    if slot5Indexing > 5:
-        isFull[5] = "FL"
-        printBoard()
-    if slot6Indexing > 5:
-        isFull[6] = "FL"
-        printBoard()
-    if slot0Indexing == 6:              # Check if the board is full and 
-        if slot0Indexing == slot1Indexing == slot2Indexing == slot3Indexing == slot4Indexing == slot5Indexing == slot6Indexing:
-            print("The board is full!")
-            quit()
+        moveIs = "X"
+       
     move = input("Choose the spot to enter your piece: ")           # Ask for input for move
     
     
