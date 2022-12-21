4e73be00278d0754ea3cafcba7f972496cbac8bc
ilovapples <89090508+ilovapples@users.noreply.github.com>
2022-12-21 15:10:36 -0600

    Updated Connect 4
    It's still on 0.7 because it still doesn't work. I am taking a break until next time I feel like messing with it.

--- connect4-0.7.py
+++ connect4-0.7.py
@@ -1,4 +1,26 @@
 # WORK IN PROGRESS
+# HAS SOME BUGS:
+"""
+#1: Doesn't show what pieces are in each column until it fills.
+
+#2: Only swaps pieces when using different columns. Pieces in column one for example will all be X or all O, and same for the other columns.
+
+
+...and probably others that I haven't found. 
+"""
+
+
+# Import os and platform.
+import os, platform
+# Check what operating system the program is running on.
+if platform.system() == "Windows":
+    def clear():
+        os.system('cls')
+else:
+    def clear():
+        os.system('clear')
+        
+# Define the variable that holds the board places.
 board = [
     ["_", "_", "_", "_", "_", "_", "_"],
     ["_", "_", "_", "_", "_", "_", "_"],
@@ -14,13 +36,14 @@ isFull = [" ", " ", " ", " ", " ", " ", " "]
 
 # Define the function to print the board.
 def printBoard():
+    clear()
     print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
     print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
     print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
     print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
     print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
     print("|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|_%s_|" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6]))
-    print("  %s  %s  %s  %s  %s  %s  %s" % (isFull[0], isFull[1], isFull[2], isFull[3], isFull[4], isFull[5], isFull[6]))
+    print("  %s   %s   %s   %s   %s   %s   %s" % (isFull[0], isFull[1], isFull[2], isFull[3], isFull[4], isFull[5], isFull[6]))
 
 stillPlaying = "y"
 while stillPlaying == "y":
@@ -31,13 +54,20 @@ while stillPlaying == "y":
     
     # Ask for move.
     move = ""
-#    while move == "" or int(move) > 7 or int(move) < 1 or board[colIndxs[int(move)-1]][int(move)-1] != "_":
     printBoard()
-    move = input("Column to place your piece (number 1-7): ")
+    move = input(f"{moveIs}'s Turn  |   Column to place your piece (number 1-7): ")
 
     if move == "" or int(move) > 7 or int(move) < 1 or board[colIndxs[int(move)-1]][int(move)-1] != "_":
         print("Invalid Move.")
+    
+    else:
+        board[colIndxs[int(move)-1]][int(move)-1] = moveIs
+        colIndxs[int(move)-1] -= 1
+        moveIsInt += 1
+        printBoard()
 
-    board[colIndxs[int(move)-1]][int(move)] = moveIs
-    printBoard()
-    moveIsInt += 1
+        if colIndxs[int(move)-1] < 0:
+            isFull[int(move)-1] = "F"
+            printBoard()
+            print("That column is full!")
+        
