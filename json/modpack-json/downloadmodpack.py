1d1b4ac3e15b1b85593e3007def45abc07a1216a
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-01 14:00:01 +0000

    Did some stuff to content the code editor (im looking at you, VSCODE)

--- json/modpack-json/downloadmodpack.py
+++ json/modpack-json/downloadmodpack.py
@@ -1,33 +1,41 @@
 from os import chdir, mkdir
-import requests
 import json
+import requests
 
-def dothething(file: str):
-    thefile = open(file, 'r').read()
-    thejson = json.loads(thefile)
+def download(file: str):
+    """
+    downloads shaders, datapacks, resourcepacks, and modpacks from 
+    .modpack.json files (format created by me)
 
-    chdir('downloaded')
-    folder = file[:file.find('.')]
-    mkdir(folder)
-    chdir(folder)
+    Args:
+        file (str): the name of the .modpack.json file to read from for modpack data information 
+    """
+    with open(file, 'r', encoding='UTF-8') as thefile:
+        thejson = json.loads(thefile)
+        chdir('downloaded')
+        folder = file[:file.find('.')]
+        mkdir(folder)
+        chdir(folder)
 
-    for projecttype in thejson['types']:
-        mkdir(projecttype)
-        chdir(projecttype)
-        for project in thejson[projecttype]:
-            print(project)
-            projectfile = requests.get(
-                thejson[projecttype][project]['directlink'], allow_redirects=True
-            )
-            extension = ''
-            if projecttype == 'mods':
-                extension = '.jar'
-            elif projecttype == 'shaders' or projecttype == 'datapacks':
-                extension = '.zip'
-            with open(thejson[projecttype][project]['slug'] + extension, 'wb') as afile:
-                afile.write(projectfile.content)
-        chdir('..')
+        for projecttype in thejson['types']:
+            mkdir(projecttype)
+            chdir(projecttype)
+            for project in thejson[projecttype]:
+                print(project)
+                projectfile = requests.get(
+                    thejson[projecttype][project]['directlink'],
+                    allow_redirects=True,
+                    timeout=10
+                )
+                extension = ''
+                if projecttype == 'mods':
+                    extension = '.jar'
+                elif projecttype == 'shaders' or projecttype == 'datapacks':
+                    extension = '.zip'
+                with open(thejson[projecttype][project]['slug'] + extension, 'wb') as afile:
+                    afile.write(projectfile.content)
+            chdir('..')
 
-filename = 'terralith-computercraft.modpack.json'
-dothething(filename)
-print(f"Your files are in downloaded/{filename[:filename.find('.')]}/")
+FILE_NAME = 'terralith-computercraft.modpack.json'
+download(FILE_NAME)
+print(f"Your files are in downloaded/{FILE_NAME[:FILE_NAME.find('.')]}/")
