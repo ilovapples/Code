bd30fd032473b22ce5ac52929b28e23229240e82
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-04 15:01:38 +0000

    updates

--- json/modpack-json/download_modpack.py
+++ json/modpack-json/download_modpack.py
@@ -1,8 +1,15 @@
 from os import chdir, mkdir, path
 import json
+import yaml
 import requests
+import time
 
-def download(file: str):
+
+def mean(data: list) -> float:
+    return sum(data)/len(data)
+
+
+def download(file: str, yamlornot: bool) -> dict:
     """
     downloads shaders, datapacks, resourcepacks, and modpacks from 
     .modpack.json files (format created by me)
@@ -10,6 +17,8 @@ def download(file: str):
     Args:
         file (str): the name of the .modpack.json file to read from for modpack data information 
     """
+    overall_start = time.time()
+
     if not path.isdir('downloaded'):
         mkdir('downloaded')
 
@@ -18,31 +27,43 @@ def download(file: str):
             mkdir('downloaded')
             
         thefile = thefiletoread.read()
+    
+    if not yamlornot:
+        thedata = json.loads(thefile)
+    else:
+        thedata = yaml.safe_load(thefile)
 
-    thejson = json.loads(thefile)
     chdir('downloaded')
     folder = file[:file.find('.')]
     if not path.isdir(folder):
         mkdir(folder)
     chdir(folder)
+    
+    if 'icon' in thedata['types']:
+        icontimestart = time.time()
+        with open('icon.png', 'wb') as theiconfile:
+            iconfile = requests.get(
+                thedata['icon'],
+                allow_redirects=True,
+                timeout=10
+            )
 
-    with open('icon.png', 'wb') as theiconfile:
-        iconfile = requests.get(
-            thejson['icon'],
-            allow_redirects=True,
-            timeout=10
-        )
+            theiconfile.write(iconfile.content)
 
-        theiconfile.write(iconfile.content)
-        print("Finished Downloading icon.")
+        icontimeend = time.time()
+        print("Finished Downloading icon in %.5f seconds.\n" % (icontimeend - icontimestart))
 
-    for projecttype in thejson['types']:
+    times = []
+
+    for projecttype in thedata['types']:
         if not path.isdir(projecttype):
             mkdir(projecttype)
         chdir(projecttype)
-        for project in thejson[projecttype]:
+
+        for project in thedata[projecttype]:
+            downloadtimestart = time.time()
             projectfile = requests.get(
-                thejson[projecttype][project]['directlink'],
+                thedata[projecttype][project]['directlink'],
                 allow_redirects=True,
                 timeout=10
             )
@@ -56,9 +77,22 @@ def download(file: str):
 
             with open(file_name, 'wb') as afile:
                 afile.write(projectfile.content)
+                downloadtimeend = time.time()
                 print(f"Downloaded:   {file_name}")
-        chdir('..')
+                print("In: %.5f seconds.\n" % (downloadtimeend - downloadtimestart))
+            times.append(downloadtimeend - downloadtimestart)
 
-JSON_FILE_NAME = 'BetterComputers.modpack.json'
-download(JSON_FILE_NAME)
-print(f"Your files are in downloaded/{JSON_FILE_NAME[:JSON_FILE_NAME.find('.')]}/")
+        chdir('..')
+    overall_end = time.time()
+    return {
+        "overall": overall_end - overall_start,
+        "downloadtimes": times
+    }
+
+if __name__ == '__main__':
+    MAIN_FILE_NAME = 'shaders.yaml'
+    thedownload = download(MAIN_FILE_NAME, True)
+    print(f"Your files are in downloaded/{MAIN_FILE_NAME[:MAIN_FILE_NAME.find('.')]}/\n")
+    print("Overall, it took %.5f seconds to download everything." % thedownload['overall'])
+    print("The average time it took for a mod/shader/resourcepack/datapack to download was %.5f seconds." % mean(thedownload['downloadtimes']))
+    print(f"Downloaded {len(thedownload['downloadtimes'])} files.")
