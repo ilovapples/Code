7f96fbb685688b54d0b439c973ba3820963e3b9a
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-05 13:02:43 +0000

    Added some C++ files.

---
+++ C++/numguess.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+    cout << "\n\033[1mNumber Guessing Game\033[0m\n";
+    int lb = 1, ub = 10;
+    srand(time(0));
+
+    int guesses = 10;
+
+    int num = rand() % (ub - lb);
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Guess a number between " << lb << " and " << ub << endl;
+        cout << "Guess: ";
+        string guessstr;
+        cin >> guessstr;
+        cout << endl;
+        int guess = stoi(guessstr);
+        if (num == guess) {
+            cout << endl << "You are correct! The number was " << num << endl;
+            int num = rand() % (ub - lb);
+            ub += 5;
+        } else if (num != guess) {
+            cout << endl << "You are incorrect. Try again.\n";
+            guesses--;
+            if (guesses == 0) {
+                cout << "You have run out of guesses." << endl;
+                return 0;
+            }
+            cout << "You have " << guesses << " guesses left.\n";
+            i--;
+        }
+    }
+    cout << endl;
+    return 0;
+}
