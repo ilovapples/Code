a3cb103ba27738fe11d37142057bb32e702b6cf9
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-11 02:56:04 +0000

    added some golang stuff

---
+++ go/test.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main() {
+	num1 := 5;
+	num2 := 8;
+	num3 := 15;
+	num4 := 108;
+
+	fmt.Printf("%-4d", num1); fmt.Printf("%4d\n", num1);
+	fmt.Printf("%-4d", num2); fmt.Printf("%4d\n", num2);
+	fmt.Printf("%-4d", num3); fmt.Printf("%4d\n", num3);
+	fmt.Printf("%-4d", num4); fmt.Printf("%4d\n", num4);
+}
