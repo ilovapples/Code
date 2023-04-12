c76a55ed0d48bf671fd6390f902f9f9eeccb6d25
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-12 00:56:11 +0000

    added some java stuff

---
+++ java/tutorial/city.java
@@ -0,0 +1,22 @@
+public class city {
+    public static void main(String[] args) {
+        class Building {
+            int rooms;
+            int floors;
+            String name;
+            // public void 
+        };
+        
+        class Person {
+            public static void init(int age_, String name_, String job_, String office_, boolean employed_, boolean married_, String spouse_) {
+                int age = age_;
+                String name = name_;
+                String job = job_;
+                String office = office_;
+                boolean employed = employed_;
+                boolean married = married_;
+                String spouse = spouse_;
+            }
+        }
+    }
+}
