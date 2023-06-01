1b0d28eef186abd9fa070e4079d2556459014198
ilovapples <ashergrossman185@gmail.com>
2023-05-31 21:31:05 -0500

    .

--- dec2bin.py
+++ dec2bin.py
@@ -11,12 +11,12 @@ def d2b(n: int):
         temps.append(int(bits/(2**i)))
     if not 1 in temps:
         temps.append(1)
-    finals = [[0, 0] for i in range(len(temps))]
     ip = [[0, i] for i in temps]
+    finals = [[0, 0] for i in range(len(temps))]
     
-    # for index, i in enumerate(ip):
-        # if 
-        # finals[index] = 
+    for index, i in enumerate(ip):
+        if 
+        finals[index] = 
     
     return [bits,ip,temps]
 
